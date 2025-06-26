58f2cf2ff9a7c76c5a4a417f45e687eace65e51b
ao627515 <ao627515@gmail.com>
2025-06-26 11:58:30 +0000

    "Added logout route with authentication middleware and updated route grouping."

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth'])->group(function () {});
+Route::middleware(['auth'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 Route::middleware(['guest'])->group(function () {
     Route::get('login', [AuthController::class, 'loginPage'])->name('login');
