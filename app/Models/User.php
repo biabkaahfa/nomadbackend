58843c10479bf3aec07788002f323dd2d0339d33
ao627515 <ao627515@gmail.com>
2025-06-26 11:48:00 +0000

    `Added JWT authentication using Tymon/JWTAuth package`

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -35,10 +36,30 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
-       
+
         'password',
         'remember_token',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * Get the attributes that should be cast.
@@ -54,14 +75,14 @@ class User extends Authenticatable
     }
     public function profil()
     {
-        return $this->belongsTo(Profils::class,'idProfil');
+        return $this->belongsTo(Profils::class, 'idProfil');
     }
-     public function garre()
+    public function garre()
     {
-        return $this->belongsTo(Garres::class,'idGarre');
+        return $this->belongsTo(Garres::class, 'idGarre');
     }
-     public function compagnie()
+    public function compagnie()
     {
-        return $this->belongsTo(Compagnies::class,'idCompagnie');
+        return $this->belongsTo(Compagnies::class, 'idCompagnie');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\JWTAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware(['auth:jwt'])->group(function () {
+    Route::post('logout', [JWTAuthController::class, 'logout']);
+    Route::post('refresh', [JWTAuthController::class, 'refresh']);
+    Route::post('me', [JWTAuthController::class, 'me']);
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::post('login', [JWTAuthController::class, 'login']);
+});
