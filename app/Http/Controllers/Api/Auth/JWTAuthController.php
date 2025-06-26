a894cc3594f2343c89d54a63ba7d018e9ec3f48a
ao627515 <ao627515@gmail.com>
2025-06-26 11:25:04 +0000

    "Added AuthController routes for login functionality"

---
+++ app/Http/Controllers/Api/Auth/JWTAuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+
+class JWTAuthController extends Controller {}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {});
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('login', [AuthController::class, 'loginPage'])->name('login');
+    Route::post('login', [AuthController::class, 'authenticate']);
 });
